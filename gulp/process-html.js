6a90371a6524548d4d14aaab11bfea4eece46f77
Daniel Hoksza <daniel.hoksza@gmail.com>
2017-02-24 18:40:25 +0100

    handlebars integration update

--- gulp/process-html.js
+++ gulp/process-html.js
@@ -20,6 +20,24 @@ gulp.task('process-html', [], function() {
 
 gulp.task('process-handlebars', [], function() {
 	var letterObject = JSON.parse(fs.readFileSync(vars.paths.data.json.src + 'letter.json', 'utf8')); // get data for the letter
+	var letterObjectNoComments = {};
+	var comment = "_former"
+	function filterObject(obj,newObj) {
+		Object.keys(obj).forEach(function (key) {
+			if (key !== comment) {
+				if (typeof(obj[key]) == "object") {
+					newObj[key] = {};
+					filterObject(obj[key],newObj[key]);
+				} else {
+					newObj[key] = obj[key];
+				}
+			}
+		});
+	}
+
+	filterObject(letterObject,letterObjectNoComments);
+	console.log(letterObjectNoComments);
+
 	options = {
 		ignorePartials: true,
 		batch: [vars.paths.handlebars.partials]
@@ -27,7 +45,7 @@ gulp.task('process-handlebars', [], function() {
 
 	return gulp.src(vars.paths.handlebars.src)
 		.pipe(plumber())
-		.pipe(handlebars(letterObject, options))
+		.pipe(handlebars(letterObjectNoComments , options))
 		.pipe(rename({extname: '.html'}))
 		.pipe(gulp.dest(vars.paths.handlebars.dest))
 		.pipe(localServer.reload());
